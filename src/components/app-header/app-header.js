3f944d938a507ea7d381c14d021ef1c4e55d52a9
Faqqy <dmitrybobkov5@gmail.com>
2022-04-06 23:20:40 +0300

    first commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,11 @@
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header">
+            <p>ajksasdasdasdhkjasd</p>
+        </div>
+    )
+}
+
+export default AppHeader;
---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import AppHeader from '../app-header/app-header';
+import './app.css';
+
+function App() {
+    return(
+        <div className="main-header">
+            <AppHeader/>
+        </div>
+    );
+}
+
+export default App;
