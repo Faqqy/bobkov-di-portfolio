0c642e3aa6a88c6e0cc49e7707b0f06784584e14
Bobkovsky <dmitrybobkov5@gmail.com>
2022-05-11 23:57:41 +0300

    comm

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -3,7 +3,25 @@ import './app-header.css';
 const AppHeader = () => {
     return (
         <div className="app-header">
-            <p>ajksasdasdasdhkjasd</p>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                        <div className="app-header_logo">
+                            <img src="/images/logo-blue.svg" alt="logo" />
+                        </div>
+                    </div>
+
+                    <div className="col-md-9">
+                        <div className="app-header_menu">
+                            <ul>
+                                <li><a href="#">Menu1</a></li>
+                                <li><a href="#">Menu2</a></li>
+                                <li><a href="#">Menu3</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
