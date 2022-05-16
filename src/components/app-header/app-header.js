b6d58c525efbb1e87ffbf5f5a5dd0d046642bf72
Bobkovsky <dmitrybobkov5@gmail.com>
2022-05-17 00:36:41 +0300

    comm 17/05/22

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -7,16 +7,16 @@ const AppHeader = () => {
                 <div className="row">
                     <div className="col-md-3">
                         <div className="app-header_logo">
-                            <img src="/images/logo-blue.svg" alt="logo" />
+                            <img src="/images/logo-black.svg" alt="logo" />
                         </div>
                     </div>
 
                     <div className="col-md-9">
                         <div className="app-header_menu">
                             <ul>
-                                <li><a href="#">Menu1</a></li>
-                                <li><a href="#">Menu2</a></li>
-                                <li><a href="#">Menu3</a></li>
+                                <li><a href="#">Обо мне</a></li>
+                                <li><a href="#">Портфолио</a></li>
+                                <li><a href="#">Контакты</a></li>
                             </ul>
                         </div>
                     </div>
